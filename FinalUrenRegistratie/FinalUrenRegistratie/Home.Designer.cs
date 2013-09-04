ea7cddb374c7f9c0292b61718895f0303f0edafd
MichaelGeurts <M.Geurts1992@live.nl>
2013-09-04 11:14:01 +0200

    label toegevoegd rechts
    R

--- FinalUrenRegistratie/FinalUrenRegistratie/Home.Designer.cs
+++ FinalUrenRegistratie/FinalUrenRegistratie/Home.Designer.cs
@@ -43,16 +43,16 @@
             this.btnWijzigUren = new System.Windows.Forms.Button();
             this.BtnUrenToevoegen = new System.Windows.Forms.Button();
             this.btnAfmelden = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnTaakwijzigen
             // 
             this.btnTaakwijzigen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTaakwijzigen.BackgroundImage")));
             this.btnTaakwijzigen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnTaakwijzigen.Location = new System.Drawing.Point(117, 199);
-            this.btnTaakwijzigen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTaakwijzigen.Location = new System.Drawing.Point(88, 162);
             this.btnTaakwijzigen.Name = "btnTaakwijzigen";
-            this.btnTaakwijzigen.Size = new System.Drawing.Size(99, 84);
+            this.btnTaakwijzigen.Size = new System.Drawing.Size(74, 68);
             this.btnTaakwijzigen.TabIndex = 33;
             this.btnTaakwijzigen.Text = "Taak wijzigen";
             this.btnTaakwijzigen.UseVisualStyleBackColor = true;
@@ -62,10 +62,9 @@
             // 
             this.btnGebruikerwijzigen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGebruikerwijzigen.BackgroundImage")));
             this.btnGebruikerwijzigen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnGebruikerwijzigen.Location = new System.Drawing.Point(117, 70);
-            this.btnGebruikerwijzigen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGebruikerwijzigen.Location = new System.Drawing.Point(88, 57);
             this.btnGebruikerwijzigen.Name = "btnGebruikerwijzigen";
-            this.btnGebruikerwijzigen.Size = new System.Drawing.Size(99, 84);
+            this.btnGebruikerwijzigen.Size = new System.Drawing.Size(74, 68);
             this.btnGebruikerwijzigen.TabIndex = 32;
             this.btnGebruikerwijzigen.Text = "Gebruiker wijzigen";
             this.btnGebruikerwijzigen.UseVisualStyleBackColor = true;
@@ -75,10 +74,9 @@
             // 
             this.btnProjectwijzigen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProjectwijzigen.BackgroundImage")));
             this.btnProjectwijzigen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnProjectwijzigen.Location = new System.Drawing.Point(380, 70);
-            this.btnProjectwijzigen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnProjectwijzigen.Location = new System.Drawing.Point(285, 57);
             this.btnProjectwijzigen.Name = "btnProjectwijzigen";
-            this.btnProjectwijzigen.Size = new System.Drawing.Size(99, 84);
+            this.btnProjectwijzigen.Size = new System.Drawing.Size(74, 68);
             this.btnProjectwijzigen.TabIndex = 31;
             this.btnProjectwijzigen.Text = "Project wijzigen";
             this.btnProjectwijzigen.UseVisualStyleBackColor = true;
@@ -88,10 +86,9 @@
             // 
             this.btnTaaktoevoegen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTaaktoevoegen.BackgroundImage")));
             this.btnTaaktoevoegen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnTaaktoevoegen.Location = new System.Drawing.Point(10, 199);
-            this.btnTaaktoevoegen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTaaktoevoegen.Location = new System.Drawing.Point(8, 162);
             this.btnTaaktoevoegen.Name = "btnTaaktoevoegen";
-            this.btnTaaktoevoegen.Size = new System.Drawing.Size(99, 84);
+            this.btnTaaktoevoegen.Size = new System.Drawing.Size(74, 68);
             this.btnTaaktoevoegen.TabIndex = 30;
             this.btnTaaktoevoegen.Text = "Taak toevoegen";
             this.btnTaaktoevoegen.UseVisualStyleBackColor = true;
@@ -101,10 +98,9 @@
             // 
             this.btnProjecttoevoegen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnProjecttoevoegen.BackgroundImage")));
             this.btnProjecttoevoegen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnProjecttoevoegen.Location = new System.Drawing.Point(273, 70);
-            this.btnProjecttoevoegen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnProjecttoevoegen.Location = new System.Drawing.Point(205, 57);
             this.btnProjecttoevoegen.Name = "btnProjecttoevoegen";
-            this.btnProjecttoevoegen.Size = new System.Drawing.Size(99, 84);
+            this.btnProjecttoevoegen.Size = new System.Drawing.Size(74, 68);
             this.btnProjecttoevoegen.TabIndex = 29;
             this.btnProjecttoevoegen.Text = "Project toevoegen";
             this.btnProjecttoevoegen.UseVisualStyleBackColor = true;
@@ -114,10 +110,9 @@
             // 
             this.btnGebruikertoevoegen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGebruikertoevoegen.BackgroundImage")));
             this.btnGebruikertoevoegen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnGebruikertoevoegen.Location = new System.Drawing.Point(10, 70);
-            this.btnGebruikertoevoegen.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGebruikertoevoegen.Location = new System.Drawing.Point(8, 57);
             this.btnGebruikertoevoegen.Name = "btnGebruikertoevoegen";
-            this.btnGebruikertoevoegen.Size = new System.Drawing.Size(99, 84);
+            this.btnGebruikertoevoegen.Size = new System.Drawing.Size(74, 68);
             this.btnGebruikertoevoegen.TabIndex = 28;
             this.btnGebruikertoevoegen.Text = "Gebruiker toevoegen";
             this.btnGebruikertoevoegen.UseVisualStyleBackColor = true;
@@ -128,10 +123,9 @@
             this.btnAfsluiten.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAfsluiten.BackgroundImage")));
             this.btnAfsluiten.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnAfsluiten.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnAfsluiten.Location = new System.Drawing.Point(117, 322);
-            this.btnAfsluiten.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAfsluiten.Location = new System.Drawing.Point(88, 262);
             this.btnAfsluiten.Name = "btnAfsluiten";
-            this.btnAfsluiten.Size = new System.Drawing.Size(99, 84);
+            this.btnAfsluiten.Size = new System.Drawing.Size(74, 68);
             this.btnAfsluiten.TabIndex = 26;
             this.btnAfsluiten.Text = "Afsluiten";
             this.btnAfsluiten.UseVisualStyleBackColor = true;
@@ -142,10 +136,9 @@
             this.lblPermissie.AutoSize = true;
             this.lblPermissie.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.lblPermissie.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblPermissie.Location = new System.Drawing.Point(269, 37);
-            this.lblPermissie.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPermissie.Location = new System.Drawing.Point(202, 30);
             this.lblPermissie.Name = "lblPermissie";
-            this.lblPermissie.Size = new System.Drawing.Size(0, 20);
+            this.lblPermissie.Size = new System.Drawing.Size(0, 17);
             this.lblPermissie.TabIndex = 25;
             // 
             // lblGebruikersnaam
@@ -153,10 +146,9 @@
             this.lblGebruikersnaam.AutoSize = true;
             this.lblGebruikersnaam.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.lblGebruikersnaam.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblGebruikersnaam.Location = new System.Drawing.Point(269, 9);
-            this.lblGebruikersnaam.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGebruikersnaam.Location = new System.Drawing.Point(202, 7);
             this.lblGebruikersnaam.Name = "lblGebruikersnaam";
-            this.lblGebruikersnaam.Size = new System.Drawing.Size(0, 20);
+            this.lblGebruikersnaam.Size = new System.Drawing.Size(0, 17);
             this.lblGebruikersnaam.TabIndex = 24;
             // 
             // label2
@@ -164,10 +156,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label2.Location = new System.Drawing.Point(113, 37);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(85, 30);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.Size = new System.Drawing.Size(73, 17);
             this.label2.TabIndex = 23;
             this.label2.Text = "Permissie:";
             // 
@@ -176,10 +167,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(113, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(85, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.Size = new System.Drawing.Size(77, 17);
             this.label1.TabIndex = 22;
             this.label1.Text = "Voornaam:";
             // 
@@ -187,10 +177,9 @@
             // 
             this.btnWijzigUren.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnWijzigUren.BackgroundImage")));
             this.btnWijzigUren.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnWijzigUren.Location = new System.Drawing.Point(380, 199);
-            this.btnWijzigUren.Margin = new System.Windows.Forms.Padding(4);
+            this.btnWijzigUren.Location = new System.Drawing.Point(285, 162);
             this.btnWijzigUren.Name = "btnWijzigUren";
-            this.btnWijzigUren.Size = new System.Drawing.Size(99, 84);
+            this.btnWijzigUren.Size = new System.Drawing.Size(74, 68);
             this.btnWijzigUren.TabIndex = 37;
             this.btnWijzigUren.Text = "Uren wijzigen";
             this.btnWijzigUren.UseVisualStyleBackColor = true;
@@ -200,10 +189,9 @@
             // 
             this.BtnUrenToevoegen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnUrenToevoegen.BackgroundImage")));
             this.BtnUrenToevoegen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BtnUrenToevoegen.Location = new System.Drawing.Point(273, 199);
-            this.BtnUrenToevoegen.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnUrenToevoegen.Location = new System.Drawing.Point(205, 162);
             this.BtnUrenToevoegen.Name = "BtnUrenToevoegen";
-            this.BtnUrenToevoegen.Size = new System.Drawing.Size(99, 84);
+            this.BtnUrenToevoegen.Size = new System.Drawing.Size(74, 68);
             this.BtnUrenToevoegen.TabIndex = 36;
             this.BtnUrenToevoegen.Text = "Uren toevoegen";
             this.BtnUrenToevoegen.UseVisualStyleBackColor = true;
@@ -213,20 +201,29 @@
             // 
             this.btnAfmelden.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAfmelden.BackgroundImage")));
             this.btnAfmelden.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnAfmelden.Location = new System.Drawing.Point(273, 322);
-            this.btnAfmelden.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAfmelden.Location = new System.Drawing.Point(205, 262);
             this.btnAfmelden.Name = "btnAfmelden";
-            this.btnAfmelden.Size = new System.Drawing.Size(99, 84);
+            this.btnAfmelden.Size = new System.Drawing.Size(74, 68);
             this.btnAfmelden.TabIndex = 38;
             this.btnAfmelden.Text = "Afmelden";
             this.btnAfmelden.UseVisualStyleBackColor = true;
             this.btnAfmelden.Click += new System.EventHandler(this.btnAfmelden_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "label3";
+            // 
             // Home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(492, 427);
+            this.ClientSize = new System.Drawing.Size(369, 347);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnAfmelden);
             this.Controls.Add(this.btnWijzigUren);
             this.Controls.Add(this.BtnUrenToevoegen);
@@ -241,6 +238,7 @@
             this.Controls.Add(this.lblGebruikersnaam);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home Scherm";
@@ -266,5 +264,6 @@
         private System.Windows.Forms.Button btnWijzigUren;
         private System.Windows.Forms.Button BtnUrenToevoegen;
         private System.Windows.Forms.Button btnAfmelden;
+        private System.Windows.Forms.Label label3;
     }
 }
