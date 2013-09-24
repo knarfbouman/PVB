aab1accab614c7d17a3a01264aa7256e9bdc4978
MichaelGeurts <M.Geurts1992@live.nl>
2013-09-24 11:40:31 +0200

    add

---
+++ Class Diagram/Class Diagram/Afbeelding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class_Diagram
+{
+    public class Afbeelding
+    {
+    
+        public int AfbeeldingID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public byte Afbeelding
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void ConvertImage()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Class Diagram/Class Diagram/Inlog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Class_Diagram
+{
+    public class Inlog
+    {
+        public int GebruikersID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Gebruikersnaam
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Wachtwoord
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public void Inloggen()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
