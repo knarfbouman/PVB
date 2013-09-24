4375b874eb119f5427545bc96fb51c966b718e69
knarfbouman <knarfbouman@live.nl>
2013-09-24 12:13:38 +0200

    :D

--- ToetsendRekenen/ToetsendRekenen/Opdracht1.aspx.designer.cs
+++ ToetsendRekenen/ToetsendRekenen/Opdracht1.aspx.designer.cs
@@ -13,12 +13,21 @@ namespace ToetsendRekenen {
     public partial class WebForm2 {
         
         /// <summary>
-        /// CheckBoxList1 control.
+        /// headerradiobutton control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBoxList CheckBoxList1;
+        protected global::System.Web.UI.WebControls.TextBox headerradiobutton;
+        
+        /// <summary>
+        /// RadioButtonList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList RadioButtonList1;
     }
 }
