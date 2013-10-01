165af57d60142f431a92b67450c3d00174d0598e
knarfbouman <knarfbouman@live.nl>
2013-10-01 12:09:47 +0200

    home & opdracht1

--- ToetsendRekenen/ToetsendRekenen/Opdracht1.aspx.cs
+++ ToetsendRekenen/ToetsendRekenen/Opdracht1.aspx.cs
@@ -11,18 +11,24 @@ namespace ToetsendRekenen
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            opdrachtlbl.Text = Convert.ToString(Session["header"]);
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            
-            
+
+            string test = Request.Form["amount"];
                 lejbel.Text = RadioButtonList1.SelectedItem.Text;
-            
+                lejbel.Text+= ", " +  SubCategorie.SelectedItem.Text;
+                lejbel.Text += ", "  ;
             }
 
         protected void SubCategorie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubCategorie_SelectedIndexChanged1(object sender, EventArgs e)
         {
 
         }
