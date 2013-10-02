518e7c7328f3370ca3755a541b23e53fab60a5e7
MichaelGeurts <M.Geurts1992@live.nl>
2013-10-02 13:57:08 +0200

    Kutzooi

--- ToetsendRekenen/ToetsendRekenen/AdminView.aspx.designer.cs
+++ ToetsendRekenen/ToetsendRekenen/AdminView.aspx.designer.cs
@@ -211,12 +211,12 @@ namespace ToetsendRekenen {
         protected global::System.Web.UI.WebControls.Label lblError;
         
         /// <summary>
-        /// GridView1 control.
+        /// DataGrid control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView DataGrid;
     }
 }
