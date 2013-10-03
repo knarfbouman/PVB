a740f0b2237ddc5403c806dfd5ad07bc7462f9fe
knarfbouman <knarfbouman@live.nl>
2013-10-03 14:31:07 +0200

    tijdlijn

--- ToetsendRekenen/ToetsendRekenen/Home.aspx.cs
+++ ToetsendRekenen/ToetsendRekenen/Home.aspx.cs
@@ -17,49 +17,54 @@ namespace ToetsendRekenen
         protected void Opdracht1_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht1.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
         }
 
         protected void Opdracht2_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht2.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
         }
 
         protected void Opdracht3_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht3.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
         }
 
         protected void Opdracht4_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht4.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
         }
 
         protected void Opdracht5_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht5.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
         }
 
         protected void Opdracht6_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht6.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
         }
 
         protected void Opdracht7_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht7.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
         }
 
         protected void Opdracht8_Click(object sender, EventArgs e)
         {
             Session.Add("header", Opdracht8.Text);
-            Response.Redirect("/opdracht1.aspx");
+            Response.Redirect("/Subcategorie.aspx");
+        }
+
+        protected void Adminbutton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/Login.aspx");
         }
     }
 }
--- ToetsendRekenen/ToetsendRekenen/Subcategorie.aspx.cs
+++ ToetsendRekenen/ToetsendRekenen/Subcategorie.aspx.cs
@@ -35,12 +35,28 @@ namespace ToetsendRekenen
                 SubCategorie.Items.Add("Gemixed");
                
             }
+
+
+            if (head == "Opdracht 2")
+            {
+
+                SubCategorie.Items.Remove("optie 1");
+                SubCategorie.Items.Remove("optie 2");
+                SubCategorie.Items.Remove("optie 3");
+                SubCategorie.Items.Remove("optie 4");
+                SubCategorie.Items.Remove("optie 5");
+                SubCategorie.Items.Add("Getallen");
+                SubCategorie.Items.Add("Kommagetallen");
+                SubCategorie.Items.Add("Breuken");        
+                SubCategorie.Items.Add("Gemixed");
+
+            }
                 
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            string header = Convert.ToString(Session["header"]);
             string getal1 = (this.Request.Form.Get("labeltest"));
            string getal2 = (this.Request.Form.Get("getal2"));
            string testhest = Convert.ToString(RadioButtonList1.SelectedItem);
@@ -53,16 +69,36 @@ namespace ToetsendRekenen
                string[] Subcatergoriearray = new string[4] { RadioButtonList1.SelectedItem.Text, SubCategorie.SelectedItem.Text, getal1, getal2 };
                
                Session.Add("subcategoriearray", Subcatergoriearray);
-               Response.Redirect("/MetSommenWerken.aspx");
-              
+               if (header == "Opdracht 1")
+               {
+                   Response.Redirect("/MetSommenWerken.aspx");
+               }
+
+               if (header == "Opdracht 2")
+               {
+                   Response.Redirect("/Tijdlijn.aspx");
+               }
+
            }
            else
            {
-               SubCategorie.Items.Remove("Erbijsommen");
-               SubCategorie.Items.Remove("Erafsommen");
-               SubCategorie.Items.Remove("Deelsommen");
-               SubCategorie.Items.Remove("Keersommen");
-               SubCategorie.Items.Remove("Gemixed");
+               string head = Convert.ToString(Session["header"]);
+               if (head == "Opdracht 1")
+               {
+
+                   SubCategorie.Items.Remove("Erbijsommen");
+                   SubCategorie.Items.Remove("Erafsommen");
+                   SubCategorie.Items.Remove("Deelsommen");
+                   SubCategorie.Items.Remove("Keersommen");
+                   SubCategorie.Items.Remove("Gemixed");
+               }
+               if (head == "Opdracht 2")
+               {
+                   SubCategorie.Items.Remove("Getallen");
+                   SubCategorie.Items.Remove("Kommagetallen");
+                   SubCategorie.Items.Remove("Breuken");
+                   SubCategorie.Items.Remove("Gemixed");
+               }
                Response.Write("<script>alert('Je bent iets vergeten te selecteren!');</script>");
                
            }
