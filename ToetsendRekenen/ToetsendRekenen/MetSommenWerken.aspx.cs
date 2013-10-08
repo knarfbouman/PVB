d07c4e71a5724f169b86c89c48e0189868b16f32
MichaelGeurts <M.Geurts1992@live.nl>
2013-10-08 11:41:40 +0200

    Merge branch 'master' of https://github.com/knarfbouman/PVB.git

--- ToetsendRekenen/ToetsendRekenen/MetSommenWerken.aspx.cs
+++ ToetsendRekenen/ToetsendRekenen/MetSommenWerken.aspx.cs
@@ -33,26 +33,29 @@ namespace ToetsendRekenen
 
         protected void naarResultaat_Click(object sender, EventArgs e)
         {
-            try
-            {
                 string aantalgoed = (this.Request.Form.Get("inputaantalgoed"));
                 Session.Add("aantalgoedsession", aantalgoed);
                 string[] subcategoriearray = (string[])Session["subcategoriearray"];
+                string categorie = subcategoriearray[4];
+                string subcategorie = subcategoriearray[1];
 
                 SqlConnection sqlConn = new SqlConnection(@"data source=www.dbss.nl;initial catalog=PVB1314-004;persist security info=True;user id=frmiok;password=ROC;MultipleActiveResultSets=True;App=EntityFramework&quot;");
-                SqlCommand sqlComm = new SqlCommand("INSERT INTO Scores (Categorienaam, Subcategorienaam, Score, Datum) Values ('" + subcategoriearray[1] + "' , '"
-                + subcategoriearray[5] + "' , '" + Convert.ToInt32(aantalgoed) * 2 + "' , '" + DateTime.Now + "'", sqlConn);
+                //SqlCommand sqlComm = new SqlCommand("INSERT INTO Scores (Categorienaam, Subcategorienaam, Score, Datum) Values ('" + subcategoriearray[1] + "' , '"
+                //+ subcategoriearray[5] + "' , '" + Convert.ToInt32(aantalgoed) * 2 + "' , '" + DateTime.Now + "'", sqlConn);
+                SqlCommand sqlComm = new SqlCommand();
+                sqlComm.Connection = sqlConn;
+                sqlComm.CommandText = "INSERT INTO Scores(Categorienaam, Subcategorienaam, Score, Datum) VALUES(@param1, @param2, @param3, @param4)";
+                sqlComm.Parameters.AddWithValue("@param1", categorie);
+                sqlComm.Parameters.AddWithValue("@param2", subcategorie);
+                sqlComm.Parameters.AddWithValue("@param3", Convert.ToInt32(aantalgoed) * 2);
+                sqlComm.Parameters.AddWithValue("@param4", DateTime.Now);  
 
                 sqlConn.Open();
                 sqlComm.ExecuteNonQuery();
                 sqlConn.Close();
 
                 Response.Redirect("/Leerlingresultaat.aspx");
-            }
-            catch(Exception)
-            {
-                Response.Write("<script>alert('Er is iets fout gegaan!');</script>");
-            }
+
         }
 
     }
