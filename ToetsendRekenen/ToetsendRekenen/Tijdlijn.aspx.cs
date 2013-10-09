0d89a7b2251976111708b9a25f90ef267eff0752
knarfbouman <knarfbouman@live.nl>
2013-10-09 08:37:51 +0200

    test
    mest

---
+++ ToetsendRekenen/ToetsendRekenen/Tijdlijn.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ToetsendRekenen
+{
+    public partial class WebForm8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            string[] Instellingenarray = (string[])Session["subcategoriearray"];
+            if (Instellingenarray != null)
+            {
+                
+
+
+
+
+
+
+            }
+            else
+            {
+                Response.Redirect("/Home.aspx");
+            }
+            
+        }
+    }
+}
