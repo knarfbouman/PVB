19b44e5cf2a3cf098f1591a7117346ba9b104720
knarfbouman <knarfbouman@live.nl>
2013-10-30 13:35:24 +0100

    Revert "Merge branch 'master' of https://github.com/knarfbouman/PVB.git"
    This reverts commit 6684a0df0e4fce0bac64619ba90c8dcfaea29ebf.

--- ToetsendRekenen/ToetsendRekenen/Getallenlijn.aspx.designer.cs
+++ ToetsendRekenen/ToetsendRekenen/Getallenlijn.aspx.designer.cs
@@ -13,24 +13,6 @@ namespace ToetsendRekenen {
     public partial class WebForm8 {
         
         /// <summary>
-        /// sterren control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image sterren;
-        
-        /// <summary>
-        /// getallenlijn control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image getallenlijn;
-        
-        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
@@ -40,6 +22,15 @@ namespace ToetsendRekenen {
         protected global::System.Web.UI.WebControls.Label Label2;
         
         /// <summary>
+        /// Label3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+        
+        /// <summary>
         /// Label5 control.
         /// </summary>
         /// <remarks>
@@ -47,6 +38,15 @@ namespace ToetsendRekenen {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label5;
+        
+        /// <summary>
+        /// Label4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
         
         /// <summary>
         /// Label1 control.
@@ -92,23 +92,5 @@ namespace ToetsendRekenen {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblvragen;
-        
-        /// <summary>
-        /// lblgoed control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblgoed;
-        
-        /// <summary>
-        /// lblerror control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblerror;
     }
 }
--- ToetsendRekenen/ToetsendRekenen/Klokkijken.aspx.cs
+++ ToetsendRekenen/ToetsendRekenen/Klokkijken.aspx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,36 +9,8 @@ namespace ToetsendRekenen
 {
     public partial class WebForm9 : System.Web.UI.Page
     {
-        Entities TRend = new Entities();
-
         protected void Page_Load(object sender, EventArgs e)
         {
-
-        }
-
-        protected void naarResultaat_Click(object sender, EventArgs e)
-        {
-            string aantalgoed = (this.Request.Form.Get("inputaantalgoed"));
-            Session.Add("aantalgoedsession", aantalgoed);
-            string categorie = "Klokkijken";
-            string subcategorieFromArray = "Analoog";
-
-            SqlConnection sqlConn = new SqlConnection(@"data source=www.dbss.nl;initial catalog=PVB1314-004;persist security info=True;user id=frmiok;password=ROC;MultipleActiveResultSets=True;App=EntityFramework&quot;");
-            //SqlCommand sqlComm = new SqlCommand("INSERT INTO Scores (Categorienaam, Subcategorienaam, Score, Datum) Values ('" + subcategoriearray[1] + "' , '"
-            //+ subcategoriearray[5] + "' , '" + Convert.ToInt32(aantalgoed) * 2 + "' , '" + DateTime.Now + "'", sqlConn);
-            SqlCommand sqlComm = new SqlCommand();
-            sqlComm.Connection = sqlConn;
-            sqlComm.CommandText = "INSERT INTO Scores(Categorienaam, Subcategorienaam, Score, Datum) VALUES(@param1, @param2, @param3, @param4)";
-            sqlComm.Parameters.AddWithValue("@param1", categorie);
-            sqlComm.Parameters.AddWithValue("@param2", subcategorieFromArray);
-            sqlComm.Parameters.AddWithValue("@param3", Convert.ToInt32(aantalgoed) * 2);
-            sqlComm.Parameters.AddWithValue("@param4", DateTime.Now);
-
-            sqlConn.Open();
-            sqlComm.ExecuteNonQuery();
-            sqlConn.Close();
-
-            Response.Redirect("/Leerlingresultaat.aspx");
 
         }
     }
